25452151437375760c92e2e7493c89e09ecc22d8
suhaig <gabor.suhai@ge.com>
2016-11-29 10:43:30 +0100

    Create the baseline for further development
    It is the first, working version of this component, however it does
    not have any test, documentation or a live demo. The code can be
    improved too.

---
+++ README.md
@@ -0,0 +1,4 @@
+# aurelia-mdl-dialog
+Aurelia wrapper for [Material Design Lite's Dialog](https://getmdl.io/components/#dialog-section) component.
+
+Work in progess!
---
+++ src/mdl-dialog-service.js
@@ -0,0 +1,99 @@
+import dialogPolyfill from 'dialog-polyfill';
+import {Container, ViewSlot, CompositionEngine, Origin} from 'aurelia-framework';
+
+export default class MdlDialogService {
+
+    static inject() {
+        return [dialogPolyfill, Container, CompositionEngine];
+    }
+
+    constructor(dialogPolyfill, container, compositionEngine) {
+        this._dialogPolyfill = dialogPolyfill;
+        this._container = container;
+        this._compositionEngine = compositionEngine;
+    }
+
+    open(configuration) {
+        if (typeof configuration.viewModel !== 'function' && typeof configuration.viewModel !== 'string') {
+            throw new Error('viewModel is a mandatory configuration property and it has to be a function or a string');
+        }
+
+        if (typeof configuration.viewModel === 'function') {
+            configuration.viewModel = Origin.get(configuration.viewModel).moduleId;
+        }
+
+        let viewSlot = new ViewSlot(document.createElement('mdl-dialog'), true);
+        let addDialogToDom = () => {
+            document.body.insertBefore(viewSlot.anchor, document.body.firstChild);
+            viewSlot.attached();
+        };
+        let removeDialogFromDom = () => {
+            if (viewSlot.isAttached) {
+                document.body.removeChild(viewSlot.anchor);
+                viewSlot.detached();
+            }
+        };
+
+        let dialogControllerPromise, resolveDialogController, rejectDialogController;
+        dialogControllerPromise = new Promise((resolve, reject) => {
+            resolveDialogController = resolve;
+            rejectDialogController = reject;
+        });
+
+        let childContainer = this._container.createChild();
+        childContainer.registerInstance(Promise, dialogControllerPromise);
+
+        let compositionContext = {
+            container: this._container,
+            childContainer: childContainer,
+            viewModel: new MdlDialogViewModel(configuration),
+            viewSlot: viewSlot
+        };
+
+        return new Promise((resolveDialogResult, rejectDialogResult) => {
+            this._compositionEngine.compose(compositionContext)
+                .then(controller => {
+                    addDialogToDom();
+                    let mdlDialog = controller.viewModel.dialog;
+                    mdlDialog.addEventListener('close', function dialogCloseListener() {
+                        this.removeEventListener('close', dialogCloseListener);
+                        removeDialogFromDom();
+                        resolveDialogResult(this.returnValue);
+                    });
+                    if (!mdlDialog.showModal) {
+                        this._dialogPolyfill.registerDialog(mdlDialog);
+                    }
+                    configuration.nonModal ? mdlDialog.show() : mdlDialog.showModal();
+                    resolveDialogController({
+                        close: function (result) {
+                            mdlDialog.close(result);
+                        }
+                    });
+                })
+                .catch(reason => {
+                    removeDialogFromDom();
+                    rejectDialogController(reason);
+                    rejectDialogResult(reason);
+                });
+        });
+    }
+}
+
+class MdlDialogViewModel {
+
+    constructor(configuration) {
+        this.configuration = configuration; // used in the view
+    }
+
+    getViewStrategy() {
+        return new InlineViewStrategy(`
+            <template>
+                <require from="jspm_packages/github/GoogleChrome/dialog-polyfill@0.4.4/dialog-polyfill.css"></require>
+            
+                <dialog mdl="dialog" ref="dialog" class.one-time="configuration.cssClass">
+                    <compose view-model.bind="configuration.viewModel" model.bind="configuration.model" containerless></compose>
+                </dialog>
+            </template>
+        `);
+    }
+}
