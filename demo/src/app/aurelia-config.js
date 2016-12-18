8f53efc56f3322ae3a397ed9b2fa0811d4c8d170
suhaig <suhai.gabor@gmail.com>
2016-12-18 14:30:51 +0100

    Add the first ever working version with webpack

--- demo/src/app/aurelia-config.js
+++ demo/src/app/aurelia-config.js
@@ -3,7 +3,7 @@ export function configure(aurelia) {
     aurelia.use
         .standardConfiguration()
         .developmentLogging()
-        .plugin('aurelia-mdl');
+//        .plugin('aurelia-mdl');
 
     aurelia.start().then(() => aurelia.setRoot());
 }
