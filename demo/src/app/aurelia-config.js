d1a505bbbc09d55f38446e771706c1bad47142d7
suhaig <suhai.gabor@gmail.com>
2017-01-03 15:11:36 +0100

    Add the first version of syntax highlighting

--- demo/src/app/aurelia-config.js
+++ demo/src/app/aurelia-config.js
@@ -3,7 +3,8 @@ export function configure(aurelia) {
     aurelia.use
         .standardConfiguration()
         .developmentLogging()
-        .plugin('aurelia-mdl');
+        .plugin('aurelia-mdl')
+        .plugin('aurelia-syntax-highlighter');
 
     aurelia.start().then(() => aurelia.setRoot());
 }
