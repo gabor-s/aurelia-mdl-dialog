c580145dab23d1bd554035c24287d89270acefe7
suhaig <suhai.gabor@gmail.com>
2017-01-04 10:50:35 +0100

    Add syntax highlight with Prism

--- demo/src/app/aurelia-config.js
+++ demo/src/app/aurelia-config.js
@@ -1,10 +1,20 @@
+import 'prismjs';
+import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
+
 export function configure(aurelia) {
+
+    /* Prism configuration can go here, e.g.:
+
+    Prism.plugins.NormalizeWhitespace.setDefaults({
+        break-lines: 72
+    });
+
+    */
 
     aurelia.use
         .standardConfiguration()
         .developmentLogging()
-        .plugin('aurelia-mdl')
-        .plugin('aurelia-syntax-highlighter');
+        .plugin('aurelia-mdl');
 
     aurelia.start().then(() => aurelia.setRoot());
 }
