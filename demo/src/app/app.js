8dfe773e02b7f8c377fc9381337d478a5c062343
suhaig <suhai.gabor@gmail.com>
2017-01-05 13:00:15 +0100

    Remove unnecessary code from app source
    Hide the code of showing the source code in the demo.

--- demo/src/app/app.js
+++ demo/src/app/app.js
@@ -1,5 +1,6 @@
 import MdlDialogService from 'aurelia-mdl-dialog';
 import {Dialog} from './dialog/dialog';
+// remove-begin
 import {default as appJsSource} from '!!raw-loader!./app.js';
 import {default as appHtmlSource} from '!!raw-loader!./app.html';
 import {default as dialogJsSource} from '!!raw-loader!./dialog/dialog.js';
@@ -8,6 +9,7 @@ import {default as dialogCssSource} from '!!raw-loader!./dialog/dialog.css';
 import {default as innerDialogJsSource} from '!!raw-loader!./dialog/inner-dialog/inner-dialog.js';
 import {default as innerDialogHtmlSource} from '!!raw-loader!./dialog/inner-dialog/inner-dialog.html';
 import {default as innerDialogCssSource} from '!!raw-loader!./dialog/inner-dialog/inner-dialog.css';
+// remove-end
 
 export class App {
 
@@ -23,10 +25,14 @@ export class App {
             textFromInnerDialog: '',
             selectedItems: []
         };
+        // remove-begin
+        // http://stackoverflow.com/questions/1979884/how-to-use-javascript-regex-over-multiple-lines/16119722#16119722
+        const JS_SOURCE_REGEXP = /\/\/ remove-begin[\s\S]*?\/\/ remove-end/g;
+        const HTML_SOURCE_REGEXP = /<!-- remove-begin -->[\s\S]*?<!-- remove-end -->/g;
         this.sources = {
             app: {
-                es2015: appJsSource,
-                html: appHtmlSource
+                es2015: appJsSource.replace(JS_SOURCE_REGEXP, ''),
+                html: '...'+appHtmlSource.replace(HTML_SOURCE_REGEXP, '')+'...'
             },
             dialog: {
                 es2015: dialogJsSource,
@@ -39,6 +45,7 @@ export class App {
                 css: innerDialogCssSource
             }
         };
+        // remove-end
     }
 
     showDialog() {
@@ -54,6 +61,7 @@ export class App {
         })
             .then(dialogResult => {
                 this.result = JSON.parse(dialogResult);
+                // https://github.com/google/material-design-lite/issues/4089
                 this.textFieldForInnerDialogText.MaterialTextfield.change(this.result.textFromInnerDialog);
                 this.textFieldForSelectedItems.MaterialTextfield.change(this.result.selectedItems.join());
             });
